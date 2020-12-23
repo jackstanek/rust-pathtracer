652281bbac51b8db09d20358d5ce4fc4d7d6c9fc
Jack Stanek <stanekjack@gmail.com>
2020-12-23 16:21:28 -0600

    Add Camera struct

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod ray;
 mod sphere;
 mod vec3;
 
+use crate::camera::Camera;
 use crate::vec3::{Vec3,Color};
 use crate::color::write_color;
 use crate::hittable::{Hittable,HittableList};
@@ -35,10 +36,7 @@ fn main() -> std::io::Result<()> {
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new(aspect_ratio, viewport_height, aspect_ratio * viewport_height, focal_length);
 
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(0.0, 0.0, -1.0, 0.5)));
@@ -51,7 +49,7 @@ fn main() -> std::io::Result<()> {
         for i in 0..width {
             let u = i as f64 / (width - 1) as f64;
             let v = (height - j) as f64 / (height - 1) as f64;
-            let r = Ray::new(&origin, &(lower_left_corner + (horizontal * u) + (vertical * v) - origin));
+            let r = camera.get_ray(u, v);
             let pixel_color = ray_color(&r, &world);
             write_color(pixel_color)
         }
