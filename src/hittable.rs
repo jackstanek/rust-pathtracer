ab727b99562105f3094c0c64be0b225c4c6cb357
Jack Stanek <stanekjack@gmail.com>
2020-12-23 12:10:33 -0600

    Add implementation of Hittablelist

--- src/hittable.rs
+++ src/hittable.rs
@@ -37,3 +37,42 @@ pub fn face_normal(ray: &Ray, outward_normal: &Vec3) -> (bool, Vec3) {
     };
     (front_face, n)
 }
+
+pub struct HittableList {
+    objects: Vec<Box<dyn Hittable>>
+}
+
+impl HittableList {
+    fn new() -> Self {
+        Self {
+            objects: Vec::new()
+        }
+    }
+
+    fn add(&mut self, object: Box<dyn Hittable>) -> () {
+        self.objects.push(object)
+    }
+
+    fn clear(&mut self) -> () {
+        self.objects.clear()
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut closest_hit: Option<HitRecord> = None;
+        for obj in &self.objects {
+            let latest_hit = obj.hit(ray, t_min, t_max);
+            match (latest_hit, closest_hit) {
+                (Some(new_hit), Some(old_hit)) => closest_hit = if new_hit.t < old_hit.t {
+                    latest_hit
+                } else {
+                    closest_hit
+                },
+                (Some(_), None) => closest_hit = latest_hit,
+                (None, _) => ()
+            }
+        }
+        closest_hit
+    }
+}
